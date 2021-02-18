8bed49b9f7f9d75aed0a6c0531cc489ed1ce9865
Frederik kohler <app-dev@frederikkohler.de>
2021-02-18 16:07:40 +0100

    Images Digital

--- src/views/DigitalSkills.vue
+++ src/views/DigitalSkills.vue
@@ -17,16 +17,30 @@
       </h2>
     </div>
     <section
-      class="w-10/12 bg-transparent md:max-w-screen-md mx-auto py-16 -mt-2 sm:mt-40 rounded-md"
+      class="w-full sm:10/12 bg-transparent md:max-w-screen-md mx-auto py-16 -mt-2 sm:mt-40 rounded-md"
+    >
+      <splide :options="options">
+        <splide-slide>
+          <img :src="require('@/assets/images/frederikkohler_dilara.jpg')" />
+        </splide-slide>
+        <splide-slide>
+          <img
+            :src="require('@/assets/images/frederikkohler_whitestars.jpg')"
+          />
+        </splide-slide>
+        <splide-slide>
+          <img :src="require('@/assets/images/frederikkohler_vue.jpg')" />
+        </splide-slide>
+        <splide-slide>
+          <img :src="require('@/assets/images/frederikkohler_effektor.jpg')" />
+        </splide-slide>
+      </splide>
+    </section>
+    <section
+      class="w-10/12 bg-transparent md:max-w-screen-md mx-auto pb-16 -mt-2 sm:mt-0 rounded-md"
     >
       <div class="flex flex-col sm:flex-row">
-        <div class="sm:w-1/2 flex items-center justify-center pb-4 sm:pb-0">
-          <img
-            :src="require(`@/assets/images/frederikkohler_digital_skills.png`)"
-            alt="Frederik Kohler Comic Art"
-          />
-        </div>
-        <div class="sm:w-1/2 flex flex-col p-4">
+        <div class="sm:w-full flex flex-col p-4">
           <h2 class="dark pb-4">
             Meine Digitale Expertice!
           </h2>
@@ -59,18 +73,47 @@
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 import Skills from '@/components/Skills.vue'
+import { Splide, SplideSlide } from '@splidejs/vue-splide'
 
+import '@splidejs/splide/dist/css/themes/splide-default.min.css'
+// or
+import '@splidejs/splide/dist/css/themes/splide-sea-green.min.css'
+// or
+import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css'
 export default {
   name: 'Print',
   components: {
     Header,
     Footer,
     Skills,
+    Splide,
+    SplideSlide,
   },
   data: function () {
     return {
       word: 'hey',
+      options: {
+        type: 'loop',
+        perPage: 2,
+        perMove: 1,
+        breakpoints: {
+          640: {
+            perPage: 1,
+          },
+        },
+        autoplay: true,
+      },
     }
   },
 }
 </script>
+
+<style scoped>
+#splide01 ul li.splide__slide {
+  padding: 1em;
+}
+
+svg {
+  fill: red !important;
+}
+</style>
